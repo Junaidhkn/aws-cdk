2ec14c84c6e7b04457bb520585a961cffb559216
junaidhkn <junaidhkniazi@gmail.com>
2023-09-07 01:32:08 +0500

    s3 bucket

--- lib/aws_cdk_cloudformation-stack.ts
+++ lib/aws_cdk_cloudformation-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkCloudformationStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		// Create an s3 bucket
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkCloudformationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		new CfnBucket(this, 'MyFirstL1Bucket', {
+			lifecycleConfiguration: {
+				rules: [
+					{
+						expirationInDays: 2,
+						status: 'Enabled',
+					},
+				],
+			},
+		});
+
+		new Bucket(this, 'MyFirstL2Bucket', {
+			lifecycleRules: [
+				{
+					expiration: cdk.Duration.days(2),
+				},
+			],
+		});
+	}
 }
